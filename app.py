e853b3c07158baef6a6096166709491816ef8de6
cellwebb <cellwebb@users.noreply.github.com>
2023-06-22 15:30:24 -0700

    add conversation history. minor tweaks

--- app.py
+++ app.py
@@ -16,51 +16,56 @@ client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f'{time.strftime("%I:%M:%S %p")} -  We have logged in as {client.user}')
+    print(f'{time.strftime("%I:%M:%S %p")} - We have logged in as {client.user}')
 
 @client.event
 async def on_message(message):
-    print(f'{time.strftime("%I:%M:%S %p")} -  Message from {message.author} in #{message.channel}: {message.content}')
+    print(f'{time.strftime("%I:%M:%S %p")} - Message from {message.author} in #{message.channel}: {message.content}')
 
     if message.author == client.user:
         return
 
-    if message.content.lower().startswith('!davefacts'):
-        # or message.channel == r'<#1119702934406566061>' \
+    if message.content.startswith('!davefacts'):
         with open('davefacts.txt', 'a') as f:
-            new_fact = message.content.replace('!davefacts', '').strip().replace('dave', 'Dave')
-            if not new_fact.endswith('.'):
-                new_fact += '.'
+            new_fact = message.content.replace('!davefacts', '').replace('dave', 'Dave').strip()
+            new_fact = new_fact if new_fact.endswith('.') else new_fact + '.'
             f.write('- ' + new_fact + '\n')
         await message.channel.send('Thanks for telling me more about Dave!')
         return
 
-    if 'hillbot' in message.content.lower() \
-        or r'<@1119312653765050429>' in message.content \
-        or client.user.mentioned_in(message):
-
+    if 'hillbot' in message.content.lower() or client.user.mentioned_in(message):
         with open('system.txt') as f:
             system_msg = f.read()
         with open('davefacts.txt') as f:
             davefacts = f.read()
 
+        print('pulling conversation history...')
+        conversation_history_generator = message.channel.history(limit=5)
+        conversation_history = []
+        async for prev_message in conversation_history_generator:
+            conversation_history.append(
+                {"role": "assistant" if prev_message.author == client.user else "user",
+                 "content": prev_message.content})
+        conversation_history.reverse()
+
+        messages = [{"role": "system", "content": system_msg + '\n' + davefacts}]
+        messages.extend(conversation_history)
+        messages[-1]["content"] = "Reply in Dave's voice. " + messages[-1]["content"]
+
+        print(messages)
         print('calling openai api...')
         for n_attempts in range(1, 6):
             try:
                 chat_completion = openai.ChatCompletion.create(
                     model="gpt-3.5-turbo",
-                    messages=[
-                        {"role": "system", "content": system_msg + '\n' + davefacts},
-                        {"role": "user", "content": message.content},
-                    ],
-                    temperature=1.3
+                    messages=messages,
+                    temperature=1.25
                 )
                 response = chat_completion.choices[0].message.content
                 break
             except (openai.error.RateLimitError, openai.error.ServiceUnavailableError) as err:
-                wait_period = 60 * n_attempts
-                await message.channel.send(
-                    f"{err} I'll try again in {wait_period} seconds!")
+                wait_period = 30 * n_attempts
+                await message.channel.send(f"{err} I'll try again in {wait_period} seconds!")
                 time.sleep(wait_period)
             except openai.error.APIError as err:
                 await message.channel.send(f"{err} I'll try re-connecting to the API!")
