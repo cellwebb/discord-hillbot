c7a714f116a0aa0c975a08a6f9f49041585c8050
cellwebb <cellwebb@users.noreply.github.com>
2023-06-26 16:26:10 -0700

    create method for chunking long conv hist messages

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import discord
 import openai
 import tiktoken
 
+from chat import chunk_long_messages
 from image import improve_image_prompt, create_image, save_image_from_url
 
 token = os.getenv("DISCORD_HILLBOT_TOKEN")
@@ -78,20 +79,7 @@ async def on_message(message):
             conversation_history.reverse()
 
             print('chunking long messages...')
-            i = 0
-            while i < len(conversation_history):
-                if len(conversation_history[i]["content"]) > 2000:
-                    encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
-                    tokenized_content = encoding.encode(conversation_history[i]["content"])
-                    num_tokens = len(tokenized_content)
-                    if num_tokens > 2000:
-                        conversation_history[i]["content"] = ''.join(tokenized_content[:2000])
-                        conversation_history.insert(
-                            i + 1,
-                            {"role": conversation_history[i]["role"],
-                            "content": tokenized_content[2000:]}
-                        )
-                i += 1
+            chunk_long_messages(conversation_history)
 
             messages = [{"role": "system", "content": system_msg + '\n' + davefacts}]
             messages.extend(conversation_history)
---
+++ chat.py
@@ -0,0 +1,18 @@
+import tiktoken
+
+def chunk_long_messages(conversation_history: List[Dict[str]]) -> None:
+    i = 0
+    while i < len(conversation_history):
+        if len(conversation_history[i]["content"]) > 2000:
+            encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
+            tokenized_content = encoding.encode(conversation_history[i]["content"])
+            num_tokens = len(tokenized_content)
+            if num_tokens > 2000:
+                conversation_history[i]["content"] = ''.join(tokenized_content[:2000])
+                conversation_history.insert(
+                    i + 1,
+                    {"role": conversation_history[i]["role"],
+                    "content": tokenized_content[2000:]}
+                )
+        i += 1
+    return
