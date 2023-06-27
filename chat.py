b2c91446bc74368353e958e06a86a56962235ce5
cellwebb <cellwebb@users.noreply.github.com>
2023-06-26 21:25:13 -0700

    create async function get_discord_conversation_history()

--- chat.py
+++ chat.py
@@ -1,6 +1,7 @@
 import tiktoken
 
-def chunk_long_messages(conversation_history: List[Dict[str]]) -> None:
+
+def chunk_long_messages(conversation_history: list[dict[str]]) -> None:
     """Splits long messages (2001+ tokens) in a conversation history to
     satisfy 'gpt-3.5-turbo' requirements."""
     i = 0
@@ -18,3 +19,18 @@ def chunk_long_messages(conversation_history: List[Dict[str]]) -> None:
                 )
         i += 1
     return
+
+
+async def get_discord_conversation_history(
+        client, message, channel_history_limit) -> list[dict[str]]:
+    """Asyncrossly pulls messages from the same channel as the message
+    and converts to format requirements of 'gpt-3.5-turbo'."""
+    conversation_history_generator = message.channel.history(limit=channel_history_limit)
+    conversation_history = []
+    async for prev_message in conversation_history_generator:
+        conversation_history.append(
+            {"role": "assistant" if prev_message.author == client.user else "user",
+            "content": prev_message.content})
+    conversation_history.reverse()
+
+    return conversation_history
