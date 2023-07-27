81c61a64a74fe9c87e9d1ab9def34a9bf561caae
cellwebb <cellwebb@users.noreply.github.com>
2023-07-27 01:15:49 -0700

    python black formatting

--- chat.py
+++ chat.py
@@ -8,9 +8,7 @@ from config import CHANNEL_HISTORY_LIMIT, TEMPERATURE
 def get_chatgpt_response(messages: list[dict[str]]) -> None:
     openai.api_key = os.getenv("OPENAI_API_KEY")
     chat_completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=messages,
-        temperature=TEMPERATURE
+        model="gpt-3.5-turbo", messages=messages, temperature=TEMPERATURE
     )
     response = chat_completion.choices[0].message.content
     return response
@@ -26,14 +24,14 @@ def chunk_messages(messages: list[dict[str]]) -> list[dict[str]]:
             tokenized_content = encoding.encode(messages[i]["content"])
             num_tokens = len(tokenized_content)
             if num_tokens > 2000:
-                messages[i]["content"] = ''.join(tokenized_content[:2000])
+                messages[i]["content"] = "".join(tokenized_content[:2000])
                 messages.insert(
                     i + 1,
-                    {"role": messages[i]["role"],
-                    "content": tokenized_content[2000:]}
+                    {"role": messages[i]["role"], "content": tokenized_content[2000:]},
                 )
         i += 1
     return messages
+
 
 async def get_channel_history(client: object, channel: object) -> list[dict[str]]:
     """Asynchronously pulls messages from a channel and converts to format
@@ -41,11 +39,12 @@ async def get_channel_history(client: object, channel: object) -> list[dict[str]
     channel_history_generator = channel.history(limit=CHANNEL_HISTORY_LIMIT)
     channel_history = []
     async for prev_message in channel_history_generator:
-        channel_history.append({
-            "role": "assistant" if prev_message.author == client.user else "user",
-            "content": prev_message.content
-        })
+        channel_history.append(
+            {
+                "role": "assistant" if prev_message.author == client.user else "user",
+                "content": prev_message.content,
+            }
+        )
     channel_history.reverse()
 
     return channel_history
-
