d33603cec337a20698c5269b42b1f7923a07f2d7
cellwebb <cellwebb@users.noreply.github.com>
2024-10-22 00:58:08 -0700

    m: finishing touches on beta image recognition

--- chat.py
+++ chat.py
@@ -24,17 +24,27 @@ async def get_chatgpt_response(
 async def get_channel_history(
     client: object, channel: object, message_limit: int, image_limit: int
 ) -> list[dict[str]]:
-    """Pulls messages from discord channel and formats for OpenAI API."""
-    channel_history_generator = channel.history(limit=message_limit)
+    """Pulls messages from discord channel and formats for OpenAI API.
 
-    image_count = 0
+    NOTE:
+    - Discord's channel history generator produces messages in reverse chronological order.
+    - OpenAI won't accept images from the assistant, so we call it a user message.
+
+    TODO:
+    - Add support for replies.
+    - Add support for embeds.
+    - Add support for messages with multiple attachments.
+    """
 
     channel_history = []
+    image_count = 0
+
+    channel_history_generator = channel.history(limit=message_limit)
     async for message in channel_history_generator:
         formatted_message = {
             "role": "assistant" if message.author == client.user else "user",
             "name": message.author.name.replace(".", "-"),
-            "content": message.content,
+            "content": [{"type": "text", "text": message.content}],
         }
 
         if (
@@ -43,16 +53,15 @@ async def get_channel_history(
             and message.attachments[0].content_type.startswith("image")
         ):
             image_count += 1
-
             if formatted_message["role"] == "user":
-                formatted_message["content"] = [
-                    {"type": "text", "text": formatted_message["content"]},
+                formatted_message["content"].append(
                     {"type": "image_url", "image_url": {"url": message.attachments[0].url}},
-                ]
+                )
             else:
                 channel_history.append(
                     {
                         "role": "user",
+                        "name": message.author.name.replace(".", "-"),
                         "content": [
                             {
                                 "type": "image_url",
@@ -63,6 +72,7 @@ async def get_channel_history(
                 )
 
         channel_history.append(formatted_message)
-    channel_history.reverse()  # Reverse to put messages in chronological order
+
+    channel_history.reverse()
 
     return channel_history
